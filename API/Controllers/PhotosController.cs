fa42ffb125122875ee9409c2c1374b4d98e66e67
Dziugas Garla <dziugas.g99@gmail.com>
2021-09-23 12:16:05 +0300

    *Better photos API

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class PhotoController : BaseApiController
+    public class PhotosController : BaseApiController
     {
         [HttpPost]
         public async Task<IActionResult> Add([FromForm] Add.Command command){
@@ -15,6 +15,11 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain (string id){
+            return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
         }
     }
 }
